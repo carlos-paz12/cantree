762b6da02484d1c3cbc51e129eff342a0200e5e4
carlos-paz12 <capz.devel@gmail.com>
2025-06-11 22:46:30 -0300

    create Tree::insert method

--- src/tree/tree.rs
+++ src/tree/tree.rs
@@ -1,4 +1,5 @@
 use super::node::Node;
+use crate::utils::pair::OrderedPair;
 
 #[derive(Debug)]
 pub struct Tree {
@@ -18,6 +19,29 @@ impl Tree {
              * ownership and moves values by default.
              */
             root: Some(Box::new(other)),
+        }
+    }
+
+    pub fn insert(&mut self, pair: OrderedPair) -> bool {
+        let new_node = Node::new(pair);
+        Self::insert_node(&mut self.root, new_node)
+    }
+
+    fn insert_node(current: &mut Option<Box<Node>>, new_node: Node) -> bool {
+        match current {
+            None => {
+                *current = Some(Box::new(new_node));
+                return true;
+            }
+            Some(existing_node) => {
+                if new_node.key < existing_node.key {
+                    Self::insert_node(&mut existing_node.left, new_node)
+                } else if new_node.key > existing_node.key {
+                    Self::insert_node(&mut existing_node.right, new_node)
+                } else {
+                    return false;
+                }
+            }
         }
     }
 }
