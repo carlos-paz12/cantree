c38d45e6d152795268d39bf1dcd2708ae1368552
carlos-paz12 <capz.devel@gmail.com>
2025-06-11 22:50:37 -0300

    add OrderedPair insertions and debug prints to Tree

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use cantree::tree::tree::Tree;
+use cantree::utils::pair::OrderedPair;
 
 fn main() {
-    let m_tree = Tree::new();
+    let mut m_tree = Tree::new();
+    println!("{:#?}\n", m_tree);
+
+    m_tree.insert(OrderedPair::new(1, 2));
+    println!("{:#?}\n", m_tree);
+
+    m_tree.insert(OrderedPair::new(0, 0));
+    println!("{:#?}\n", m_tree);
+
+    m_tree.insert(OrderedPair::new(3, 9));
     println!("{:#?}", m_tree);
 }
