1b0fdeeb5d9f94bd0c2906b2990078ecbaec5e6d
carlos-paz12 <capz.devel@gmail.com>
2025-06-11 21:48:54 -0300

    add method Node::new with automatic key generation

--- src/tree/node.rs
+++ src/tree/node.rs
@@ -1,3 +1,4 @@
+use crate::utils::cantor::gen_key;
 use crate::utils::pair::OrderedPair;
 
 #[derive(Debug)]
@@ -10,4 +11,16 @@ pub struct Node {
      */
     pub left: Option<Box<Node>>,
     pub right: Option<Box<Node>>,
+}
+
+impl Node {
+    pub fn new(_pair_: OrderedPair) -> Self {
+        let _key_ = gen_key(_pair_.first, _pair_.second);
+        Node {
+            key: _key_,
+            pair: _pair_,
+            left: None,
+            right: None,
+        }
+    }
 }
