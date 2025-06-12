36075be75dc8b327bc58cd2d69c13b73e424c5f2
carlos-paz12 <capz.devel@gmail.com>
2025-06-11 21:46:39 -0300

    add tests to `gen_key` function at cartor.rs

--- src/utils/cantor.rs
+++ src/utils/cantor.rs
@@ -3,3 +3,26 @@ pub fn gen_key(p: u32, q: u32) -> u64 {
     let sum = p + q;
     return (sum * (sum + 1)) / 2 + q;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    // [!] Test key generation for known pairs.
+    fn test_some_pairs() {
+        assert_eq!(gen_key(0, 0), 0);
+        assert_eq!(gen_key(1, 0), 1);
+        assert_eq!(gen_key(0, 1), 2);
+        assert_eq!(gen_key(1, 1), 4);
+        assert_eq!(gen_key(2, 2), 12);
+    }
+
+    #[test]
+    // [!] Test non-commutativity of pairs.
+    fn test_commutativity_failure() {
+        // [!] `gen_key` is NOT commutative, i.e., `gen_key(p, q)` != `gen_key(q, p)`.
+        assert_ne!(gen_key(0, 1), gen_key(1, 0));
+        assert_ne!(gen_key(1, 2), gen_key(2, 1));
+    }
+}
