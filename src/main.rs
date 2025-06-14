d304525a220d660539680cd9fa730fc7eede6abf
carlos-paz12 <capz.devel@gmail.com>
2025-06-14 11:17:40 -0300

    informal test of the BinarySearchTree::remove method

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 use cantree::{tree::bst::BinarySearchTree, utils::pair::OrderedPair};
 
-fn main() {
+fn main()
+{
     let mut m_tree = BinarySearchTree::new();
     println!("{:#?}\n", m_tree);
 
-    m_tree.insert(OrderedPair::new(1, 2));
-    println!("{:#?}\n", m_tree);
-
     m_tree.insert(OrderedPair::new(0, 0));
+    m_tree.insert(OrderedPair::new(1, 2));
+    m_tree.insert(OrderedPair::new(0, 1));
+    m_tree.insert(OrderedPair::new(3, 9));
+
     println!("{:#?}\n", m_tree);
 
-    m_tree.insert(OrderedPair::new(3, 9));
-    println!("{:#?}", m_tree);
-
-    if let Some(p) = m_tree.search(87) {
-        println!("{:#?}", p); // [!] Espera como saída o último par adicionado na árvore.
+    if let Some(node) = m_tree.remove(&87)
+    {
+        println!("{:#?}", node);
     }
+
+    println!("{:#?}\n", m_tree);
+    if let Some(node) = m_tree.remove(&8)
+    {
+        println!("{:#?}", node);
+    }
+
+    println!("{:#?}\n", m_tree);
 }
