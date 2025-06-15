6ec341549f62ecedbba1d1b62b44854749e22f92
carlos-paz12 <capz.devel@gmail.com>
2025-06-15 11:45:52 -0300

    change formatting style

--- src/tree/node.rs
+++ src/tree/node.rs
@@ -4,26 +4,26 @@ use crate::utils::pair::OrderedPair;
 #[derive(Debug, Clone)]
 pub struct Node
 {
-    pub key: u64,
-    pub pair: OrderedPair,
-    /* [!]
-     * I think it's possible to use `*mut Node` or `&mut Node`...
-     * But I don't know
-     */
-    pub left: Option<Box<Node>>,
-    pub right: Option<Box<Node>>,
+  pub key: u64,
+  pub pair: OrderedPair,
+  /* [!]
+   * I think it's possible to use `*mut Node` or `&mut Node`...
+   * But I don't know
+   */
+  pub left: Option<Box<Node>>,
+  pub right: Option<Box<Node>>,
 }
 
 impl Node
 {
-    pub fn new(ordered_pair: OrderedPair) -> Self
-    {
-        let key = gen_key(&ordered_pair);
-        Node {
-            key: key,
-            pair: ordered_pair,
-            left: None,
-            right: None,
-        }
+  pub fn new(ordered_pair: OrderedPair) -> Self
+  {
+    let key = gen_key(&ordered_pair);
+    Node {
+      key: key,
+      pair: ordered_pair,
+      left: None,
+      right: None,
     }
+  }
 }
--- src/utils/cantor.rs
+++ src/utils/cantor.rs
@@ -2,39 +2,39 @@ use super::pair::OrderedPair;
 
 pub fn gen_key(pair: &OrderedPair) -> u64
 {
-    let (p, q) = (pair.first as u64, pair.second as u64);
-    let sum = p + q;
-    return (sum * (sum + 1)) / 2 + q;
+  let (p, q) = (pair.first as u64, pair.second as u64);
+  let sum = p + q;
+  return (sum * (sum + 1)) / 2 + q;
 }
 
 #[cfg(test)]
 mod tests
 {
-    use super::*;
+  use super::*;
 
-    #[test]
-    // [!] Test key generation for known pairs.
-    fn test_some_pairs()
-    {
-        assert_eq!(gen_key(&OrderedPair::new(0, 0)), 0);
-        assert_eq!(gen_key(&OrderedPair::new(1, 0)), 1);
-        assert_eq!(gen_key(&OrderedPair::new(0, 1)), 2);
-        assert_eq!(gen_key(&OrderedPair::new(1, 1)), 4);
-        assert_eq!(gen_key(&OrderedPair::new(2, 2)), 12);
-    }
+  #[test]
+  /// Test key generation for known pairs.
+  fn test_some_pairs()
+  {
+    assert_eq!(gen_key(&OrderedPair::new(0, 0)), 0);
+    assert_eq!(gen_key(&OrderedPair::new(1, 0)), 1);
+    assert_eq!(gen_key(&OrderedPair::new(0, 1)), 2);
+    assert_eq!(gen_key(&OrderedPair::new(1, 1)), 4);
+    assert_eq!(gen_key(&OrderedPair::new(2, 2)), 12);
+  }
 
-    #[test]
-    // [!] Test non-commutativity of pairs.
-    fn test_commutativity_failure()
-    {
-        // [!] `gen_key` is NOT commutative, i.e., `gen_key(p, q)` != `gen_key(q, p)`.
-        assert_ne!(
-            gen_key(&OrderedPair::new(0, 1)),
-            gen_key(&OrderedPair::new(1, 0))
-        );
-        assert_ne!(
-            gen_key(&OrderedPair::new(1, 2)),
-            gen_key(&OrderedPair::new(2, 1))
-        );
-    }
+  #[test]
+  /// Test non-commutativity of pairs.
+  /// `gen_key` is NOT commutative, i.e., `gen_key(p q)!=gen_key(q,p)`.
+  fn test_commutativity_failure()
+  {
+    assert_ne!(
+      gen_key(&OrderedPair::new(0, 1)),
+      gen_key(&OrderedPair::new(1, 0))
+    );
+    assert_ne!(
+      gen_key(&OrderedPair::new(1, 2)),
+      gen_key(&OrderedPair::new(2, 1))
+    );
+  }
 }
